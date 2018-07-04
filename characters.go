2e7dc7fdea859ef2ae3b77cbc7b512129c734672
Kotone Itaya <kotone@sfc.keio.ac.jp>
2018-07-04 14:15:01 +0900

    Add NotLatin parser

--- characters.go
+++ characters.go
@@ -22,6 +22,42 @@ var (
 	Space  = Bytes(whitespace...)
 )
 
+func inRange(b, a, z byte) bool {
+	return a <= b && b <= z
+}
+
+func isUpper(b byte) bool {
+	return inRange(b, 'A', 'Z')
+}
+
+func isLower(b byte) bool {
+	return inRange(b, 'a', 'z')
+}
+
+func isDigit(b byte) bool {
+	return inRange(b, '0', '9')
+}
+
+func isLatin(b byte) bool {
+	return isUpper(b) || isLower(b) || isDigit(b)
+}
+
+// NotLatin matches a non-latin character.
+func NotLatin(state *State, result *Result) error {
+	if err := state.Want(1); err != nil {
+		return err
+	}
+
+	b := state.Buffer[state.Index]
+	if isLatin(b) {
+		return NewMismatchError("NotLatin", []byte("non-latin"), state.Position)
+	}
+
+	result.Value = b
+	state.Advance(1)
+	return nil
+}
+
 // Escape sequences (JavaScript based)
 var (
 	SingleQuote    = ByteSlice([]byte{'\\', '\''}).Bind(byte('\''))
