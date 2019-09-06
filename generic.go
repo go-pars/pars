f094e94b33a57c13949553ebd66f3743b92d871a
Kotone Itaya <kotone@sfc.keio.ac.jp>
2019-09-06 11:12:45 +0900

    Add Word parser and fix erroneous state manipulation

--- generic.go
+++ generic.go
@@ -1,6 +1,8 @@
 package pars
 
-import "io"
+import (
+	"io"
+)
 
 // Line matches a single line of text.
 func Line(state *State, result *Result) error {
@@ -19,12 +21,38 @@ func Line(state *State, result *Result) error {
 			return err
 		}
 
-		state.Advance(1)
-
 		if state.Buffer[state.Index] == '\n' {
 			result.Value = string(state.Buffer[start:state.Index])
 			state.Advance(1)
 			return nil
 		}
+
+		state.Advance(1)
+	}
+}
+
+// Word matches a group of non-whitespace characters.
+func Word(state *State, result *Result) error {
+	start := state.Index
+
+	state.Mark()
+
+	for {
+		if err := state.Want(1); err != nil {
+			if err == io.EOF {
+				result.Value = string(state.Buffer[start:state.Index])
+				return nil
+			}
+
+			state.Jump()
+			return err
+		}
+
+		if isWhitespace(state.Buffer[state.Index]) {
+			result.Value = string(state.Buffer[start:state.Index])
+			return nil
+		}
+
+		state.Advance(1)
 	}
 }
--- generic_test.go
+++ generic_test.go
@@ -31,3 +31,27 @@ func BenchmarkLine(b *testing.B) {
 		p(s, pars.VoidResult)
 	}
 }
+
+func TestWord(t *testing.T) {
+	r := pars.Result{}
+
+	t.Run("matches word", func(t *testing.T) {
+		e := "hello"
+		b := " world"
+		s := pars.NewState(strings.NewReader("hello world"))
+		err := pars.Word(s, &r)
+		pars.Cut(s, &r)
+		require.NoError(t, err)
+		require.IsType(t, e, r.Value)
+		require.Equal(t, e, r.Value)
+		require.Equal(t, b, string(s.Buffer))
+	})
+}
+
+func BenchmarkWord(b *testing.B) {
+	p := pars.Dry(pars.Word)
+	s := pars.NewState(strings.NewReader("hello world"))
+	for i := 0; i < b.N; i++ {
+		p(s, pars.VoidResult)
+	}
+}
